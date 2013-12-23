eaee38c1f0b8d49fefdeafc1e47f1ac4e5460560
Dibe Zackaria <dibe.zack@gmail.com>
2013-12-23 19:05:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 ROOT_UID="0"
+home=$HOME
 
 if [ "$UID" -ne "$ROOT_UID" ] ; then
     echo "You must be root to do that!"
     exit 1
 fi
 
-cp ./i3/config ~/.i3/
+cp ./i3/config $home/.i3/
 cp ./i3/i3status.conf /etc/
 
-cp ./X/.Xdefaults ~/
-cp ./zsh/.zshrc ~/
-cp ./zsh/crunch.zsh-theme ~/.oh-my-zsh/themes/
+cp ./X/.Xdefaults $home/
+cp ./zsh/.zshrc $home/
+cp ./zsh/crunch.zsh-theme $home/.oh-my-zsh/themes/
 
 cp -r ./slim/binary /usr/share/slim/themes/
 cp ./slim/slim.conf /etc/
